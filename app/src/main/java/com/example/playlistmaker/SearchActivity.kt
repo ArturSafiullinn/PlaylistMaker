1c1bb51550945b72fb49fd87ed347c25d77af78c
ArturSafiullin <art22061993@gmail.com>
2025-03-17 23:33:13 +0400

    Домашнее задание 9го спринта

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,19 +1,78 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 
 class SearchActivity : AppCompatActivity() {
+    private var searchText: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backButton = findViewById<ImageView>(R.id.back_to_main_menu)
+        backButton.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        val searchInput = findViewById<EditText>(R.id.search_input)
+        searchInput?.setupClearButtonWithAction()
+
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                searchText = editable.toString()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_text", searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString("search_text", "") ?: ""
+        findViewById<EditText>(R.id.search_input).setText(searchText)
+    }
+
+    private fun EditText.setupClearButtonWithAction() {
+        val searchIcon = ContextCompat.getDrawable(context, R.drawable.search)
+        val clearIcon = ContextCompat.getDrawable(context, R.drawable.ic_clear)
+        setCompoundDrawablesWithIntrinsicBounds(searchIcon, null, null, null)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                val clearDrawable = if (editable?.isNotEmpty() == true) clearIcon else null
+                setCompoundDrawablesWithIntrinsicBounds(searchIcon, null, clearDrawable, null)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        })
+
+        setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val touchPositionX = event.rawX
+                val drawableEndX = this.right - this.compoundPaddingRight
+                if (touchPositionX >= drawableEndX) {
+                    this.setText("")
+                    return@setOnTouchListener true
+                }
+            }
+            return@setOnTouchListener false
         }
     }
 }
