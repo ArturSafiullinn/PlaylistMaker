21ddc025f4dd7410e8bb049549a78265460a3f85
ArturSafiullin <art22061993@gmail.com>
2025-05-06 10:12:51 +0400

    Домашнее задание 12 спринта

--- app/src/main/java/com/example/playlistmaker/TrackActivity.kt
+++ app/src/main/java/com/example/playlistmaker/TrackActivity.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackActivity : AppCompatActivity() {
 
@@ -21,11 +22,18 @@ class TrackActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.value_genre).text = track.genre
         findViewById<TextView>(R.id.value_country).text = track.country
         findViewById<TextView>(R.id.playback_time).text = track.trackTime
+        findViewById<TextView>(R.id.value_duration).text = track.trackTime
 
         val cover = findViewById<ImageView>(R.id.album_cover)
+        val cornerRadiusDp = 8
+        val density = this.resources.displayMetrics.density
+        val cornerRadiusPx = (cornerRadiusDp * density).toInt()
         Glide.with(this)
             .load(track.artworkUrl.replace("100x100bb.jpg", "512x512bb.jpg"))
             .placeholder(R.drawable.placeholder)
+            .error(R.drawable.erro_placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(cornerRadiusPx))
             .into(cover)
 
         findViewById<ImageView>(R.id.back_button).setOnClickListener {
